2c500efdb09eafc452d1d33732b0b0be20774b41
Raghda Gamal <raghdagamal77@gmail.com>
2017-07-17 11:49:37 +0200

    ngx-translate

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,27 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {TranslateModule,TranslateLoader} from '@ngx-translate/core';
+import {TranslateHttpLoader} from '@ngx-translate/http-loader';
+import {HttpModule, Http} from '@angular/http';
+
+export function CreateTranslateloader(http:Http){
+	return new TranslateHttpLoader(http,"i18n/",".json");
+}
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule,HttpModule,
+    AppRoutingModule,
+    TranslateModule.forRoot({loader:{useFactory:(CreateTranslateloader),provide:TranslateLoader,deps:[Http]}})
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+
+ }
