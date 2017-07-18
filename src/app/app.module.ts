1f50f9b22e34f119a15aae5416d27f993d379aa5
Abdulrahman Alaa <abdulrahmanalaa@hotmail.com>
2017-07-18 16:02:40 +0200

    fixed merging issues

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,6 @@
+import { InterceptableHttpService } from './Infrastructure/HttpInterceptor.interface';
+import { CustomComponentModule } from './custom-component/custom-component.module';
+import { InputTextComponent } from './custom-component/input-text/input-text.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
@@ -9,6 +12,17 @@ import {HttpModule, Http} from '@angular/http';
 
 export function CreateTranslateloader(http:Http){
 	return new TranslateHttpLoader(http,"i18n/",".json");
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { HttpModule, Http, XHRBackend, RequestOptions } from '@angular/http';
+import { APP_INITIALIZER } from '@angular/core';
+import { ConfigService } from './config/config.service';
+import { environment } from '../environments/environment';
+export function CreateTranslateloader(http: Http) {
+  return new TranslateHttpLoader(http, "i18n/", ".json");
+}
+export function ConfigLoader(configService: ConfigService) {
+  return () => configService.load(environment.configFile + 'config.json');
 }
 
 
@@ -20,8 +34,26 @@ export function CreateTranslateloader(http:Http){
     BrowserModule,HttpModule,
     AppRoutingModule,
     TranslateModule.forRoot({loader:{useFactory:(CreateTranslateloader),provide:TranslateLoader,deps:[Http]}})
+    FormsModule,
+    ReactiveFormsModule,
+    CustomComponentModule,
+    TranslateModule.forRoot({ loader: { useFactory: (CreateTranslateloader), provide: TranslateLoader, deps: [Http] } })
   ],
-  providers: [],
+  providers: [
+    {
+      provide: Http,
+      useFactory: (backend, defaultOptions) => new InterceptableHttpService(backend, defaultOptions),
+      deps: [XHRBackend, RequestOptions]
+    },
+    ConfigService,
+    {
+      provide: APP_INITIALIZER,
+      useFactory: ConfigLoader,
+      deps: [ConfigService],
+      multi: true
+    }
+
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {
